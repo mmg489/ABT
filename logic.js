b4ce302b64d18f660e37332539ac32809fddbea6
Margarita <m_mg489@outlook.com>
2020-08-03 22:43:20 -0400

    added form for book input and started logic

---
+++ logic.js
@@ -0,0 +1,12 @@
+//goals of the logic
+//user will input the name of the Book they are reading
+//will be saved to local storage
+let bookName = document.querySelector("#bookName");
+console.log(bookName);
+//Ask user what they are reading
+
+//user will then have the option to save character names in a list
+
+//user will be able to provide a summary as they read (notes of their last chapter read etc)
+
+//user will store the chapter and minute mark they were on
