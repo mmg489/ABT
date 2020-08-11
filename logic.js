f56491d02bb3a743796173c879a8935318224613
Margarita <m_mg489@outlook.com>
2020-08-11 01:42:42 -0400

    attempt two for getting the localStorage to store Data inputted

--- logic.js
+++ logic.js
@@ -1,7 +1,6 @@
 //goals of the logic
 //user will input the name of the Book they are reading
 function buttonPressed() {
-  // document.write ( document.myForm.elements[0].value );
   var book = document.getElementById("bookName");
 
   document.getElementById("currentBook").append(book.value);
@@ -19,25 +18,14 @@ function buttonPressed() {
 
   var bookNote = document.getElementById("bookNotes");
   document.getElementById("notes").append(bookNote.value);
+
+  //store the name of the book
+  //will be saved to local storage
+  localStorage.setItem("bookName", JSON.stringify(book));
+  localStorage.setItem("chapterNumber", JSON.stringify(chapter));
+  localStorage.setItem("minuteMarker", JSON.stringify(minute));
+  localStorage.setItem("characterList", JSON.stringify(character));
+  localStorage.setItem("bookNotes", JSON.stringify(bookNote));
+
+  console.log(localStorage);
 }
-//identify when the user submits the book name
-//store the name of the book
-localStorage.setItem("book_name", JSON.stringify(minuteMarker));
-localStorage.setItem("chapter_number", "ReadyPlayerOne");
-localStorage.setItem("minute_mark", "ReadyPlayerOne");
-localStorage.setItem("characters", "ReadyPlayerOne");
-localStorage.setItem("book_notes", "ReadyPlayerOne");
-console.log(localStorage);
-// if (localStorage(Storage) !== "undefined") {
-//   console.log("Storage is confirmed");
-// } else {
-//   console.log("Sorry! No web storage support");
-// }
-
-//will be saved to local storage
-
-//user will then have the option to save character names in a list
-
-//user will be able to provide a summary as they read (notes of their last chapter read etc)
-
-//user will store the chapter and minute mark they were on
