21b270beaaf39bd6b27b4c541cfe1bb6c3fa5cf7
Margarita <m_mg489@outlook.com>
2020-08-25 23:21:57 -0400

    logic for data storage

--- logic.js
+++ logic.js
@@ -38,3 +38,13 @@ function saveBook() {
   };
   console.log(bookData);
 }
+
+/ Store data
+var someData = 'The data that I want to store for later.';
+localStorage.setItem('myDataKey', someData);
+
+// Get data
+var data = localStorage.getItem('myDataKey');
+
+// Remove data
+localStorage.removeItem('myDatakey');
